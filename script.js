3b93a2361abc8187030e121ffeb581d8295ae183
Miraj Mainali <merazzmainali05@gmail.com>
2024-11-27 13:07:46 +1100

    added the js logic for handling the number a and symbol based on the user imput

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log('Happy developing ✨')
+
+
+
+let screenValue='0'
+function  buttonClick(value){
+   if(isNaN(parseInt(value))){ //
+       handleSymbol()
+   }else{
+       handleNumber()
+   }
+    
+}
+
+function handleNumber(number){
+    console.log('number')
+}
+
+function handleSymbol(symbol){
+    console.log('symbol')
+    
+}
+
+function init(){
+    document
+        .querySelector('.calc-buttons')
+        .addEventListener('click',function(event){
+        buttonClick(event.target.innerText);
+            
+    })
+}
+
+init()
